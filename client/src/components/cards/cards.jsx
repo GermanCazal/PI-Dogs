ca0452d4d134a6466f883bb548f767a1126c8b8d
GermanCazal <germancazal124@gmail.com>
2023-06-12 13:06:36 -0300

    trabajando el front

--- client/src/components/cards/cards.jsx
+++ client/src/components/cards/cards.jsx
@@ -1,13 +1,13 @@
 import "./cards.styles.css"
 
 function Cards({user}) {
-  const{imagen,nombre,temperamento,peso}= user;
+  const{image,name,temperament,weight}= user;
     return (
       <div className="card-container">
-       <h1>{imagen}</h1>
-       <h2>{nombre}</h2>
-       <p>{temperamento}</p>
-       <p>{peso}</p>
+       <h1>{name}</h1>
+       <img src={image.url} alt={name} />
+       <p>{temperament}</p>
+       <p>{weight.imperial}</p>
       </div>
     );
   }
