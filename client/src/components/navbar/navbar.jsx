493ef78357168be4197079701ac602f4d5dc03cd
GermanCazal <germancazal124@gmail.com>
2023-06-12 20:26:50 -0300

    useEffect

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,10 +1,10 @@
-function Navbar() {
+function Navbar({handleSubmit,handleChange}) {
     return (
       <div className="search-box">
         <form>
-          <input placeholder="Busqueda" />
+          <input onChange={handleChange} placeholder="Busqueda" />
           <br />
-          <button>Buscar</button>
+          <button onClick={handleSubmit}>Buscar</button>
         </form>
       </div>
     );
--- client/src/views/home/home.jsx
+++ client/src/views/home/home.jsx
@@ -2,29 +2,46 @@ import Navbar from "../../components/navbar/navbar";
 import {useDispatch,useSelector} from "react-redux"
 import CardList from "../../components/cardlist/CardList";
 import"./home.styles.css"
-import { useEffect,} from "react";
+import { useEffect, useState,} from "react";
 
 import { getUsers } from "../../redux/actions";
-
-
-
 
 
 
 function Home() {
   const dispatch = useDispatch()
   const allUsers = useSelector((state)=>state.allUsers)
+ const [filterd, setFilterd]= useState([])
+ const [searchSting,setSearchString]=useState("")
 
-  useEffect(()=>{
-    dispatch(getUsers())
-  },[])
+
+   useEffect(()=>{
+    dispatch(getUsers());
+    setFilterd(allUsers)
+   },[dispatch, allUsers])
+
+
+const handleChange =(event) =>{
+setSearchString(event.target.value.toLowerCase());
+}
+
+const handleSubmit =(event)=>{
+  event.preventDeafault();
+  if(searchSting !==""){
+    const filter = filterd.filter(user => user.name.toLowerCase().includes(searchSting))
+    return setFilterd(filter)
+  }
+  setFilterd(allUsers)
+}
+
+
 
 
     return (
       <div className="home">
         <h1 className="home-title ">Estoy en home</h1>
-        <Navbar/>
-        <CardList allUsers={allUsers}/>
+        <Navbar handleSubmit={handleSubmit} handleChange={handleChange}/>
+        <CardList allUsers={filterd}/>
       </div>
     );
   }
